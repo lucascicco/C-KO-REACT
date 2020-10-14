b08b73e7410cc5409b33c2a4ce74a0cdf8eb2ffc
lucacicco <lucasciccomy@gmail.com>
2020-10-14 19:25:46 -0300

    created routes and layouts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
+
+import './config/ReactotronConfig';
+
 import Routes from './routes';
+import history from './services/history';
+
+import GlobalStyle from './styles/global';
 
 import './App.css';
 
 function App() {
-  return <Routes />;
+  return (
+    <Router history={history}>
+      <Routes />
+      <GlobalStyle />
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/routes/Route.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+import DefaultLayout from '../pages/_layouts/default';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate = false,
+  ...rest
+}) {
+  const signed = false;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="homepage" />;
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={(props) => (
+        <DefaultLayout>
+          <Component {...props} />
+        </DefaultLayout>
+      )}
+    />
+  );
+}
+
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
